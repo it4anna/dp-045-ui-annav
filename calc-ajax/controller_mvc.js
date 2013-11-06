03f3f9c36d0e53b40adec731e6db637a8b8ebf68
AnnaVostrykova <it4anna@gmail.com>
2013-11-06 11:17:23 +0200

    ok

---
+++ calc-ajax/controller_mvc.js
@@ -0,0 +1,52 @@
+"use strict";
+function Controller () {
+    var myCalc= new Calculator(),
+        operand='',
+        result = document.getElementById("result_input"),
+        first_value = document.getElementById("first_value"),
+        second_value = document.getElementById("second_value");
+
+    this.setField1 = function () {
+        operand = "first_value";
+    };
+
+    this.setField2 = function () {
+        operand = "second_value";
+    };
+
+    this.changeField = function () {
+        if( operand === '' ) {
+            alert( "Set field" );
+        } else {
+            document.getElementById(operand).value += this.value;
+        }
+    };
+    this.erase = function () {
+        first_value.value = "";
+        second_value.value = "";
+        result.value = "";
+
+    };
+
+    this.plus = function () {
+//        console.log("plus!");
+        myCalc.plus ( Number(first_value.value), Number(second_value.value) );
+    };
+
+    this.minus = function () {
+//        console.log("minus!");
+        myCalc.minus ( Number(first_value.value), Number(second_value.value) );
+    };
+
+    this.division = function () {
+//        console.log("division!");
+        myCalc.division ( Number(first_value.value), Number(second_value.value) );
+    };
+
+    this.multiply = function () {
+//        console.log("multily!");
+        myCalc.multiply ( Number(first_value.value), Number(second_value.value) );
+    };
+
+    return this;
+}
---
+++ calc-ajax/model_mvc.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function Calculator () {
+
+
+    this.plus = function ( oper1, oper2 ) {
+        var my_ajax = new Ajax ();
+        my_ajax.ajaxDialogue( oper1, oper2, "http://test1.ru/add.php");
+    };
+
+    this.minus = function ( oper1, oper2 ) {
+        var my_ajax = new Ajax ();
+        my_ajax.ajaxDialogue( oper1, oper2, "http://test1.ru/sub.php");
+    };
+
+    this.multiply = function ( oper1, oper2 ) {
+        var my_ajax = new Ajax ();
+        my_ajax.ajaxDialogue( oper1, oper2, "http://test1.ru/mul.php");
+    };
+
+    this.division = function ( oper1, oper2 ) {
+        var my_ajax = new Ajax ();
+        if(oper2 == 0) {
+            document.getElementById( "" ).value = "Error division by zero";
+        }else {
+            my_ajax.ajaxDialogue( oper1, oper2, "http://test1.ru/div.php");
+        }
+    };
+    return this;
+}
