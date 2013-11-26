942f09f17c81973ee47b31d615280544f811398c
AnnaVostrykova <it4anna@gmail.com>
2013-11-26 23:06:06 +0200

    ror cats

---
+++ rails/first/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+First::Application.config.secret_token = '6e17ed02f5f1a314f11e6a7e2fd845a7ff1647b19caedcf14b4b3cfb4b80bbc080e10c7376fdbcc04ebb32396c15b0c2f97b43edb58fec7e6dd078ce6211707e'
---
+++ rails/first/public/js/bb.js
@@ -0,0 +1,67 @@
+$(function (){
+
+var Cat = Backbone.Model.extend({
+    defaults: {
+        name : "undefined",
+        color : "undefined",
+		id : "unset"
+    }
+//	url:"/cats/+id  //dlya kajdogo kota
+    }),
+
+
+
+CatsCollection = Backbone.Collection.extend({
+    initialize: function() {
+ /*       this.add(new Cat({"name": "Murka", "color": "white"}));
+        this.add(new Cat({"name": "Murzik", "color": "grey"}));
+        this.add(new Cat({"name": "Barsic", "color": "green"}));
+		*/	
+		this.fetch();
+      },
+	  
+    model : Cat,
+	url: "/cats"
+	
+	
+}),
+
+
+    CatView = Backbone.View.extend({
+    className: "cat",
+
+    template: _.template($("#kitty_tpl").html()),
+
+    render: function() {
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
+    }
+}),
+
+    CatCollectionView = Backbone.View.extend({
+        initialize: function() {
+            this.collection = new CatsCollection();
+        },
+
+        el: $("#cats_action"),
+
+        events: {
+            "click": "render"
+        },
+
+        addCat: function(cat) {
+            var view = new CatView({model: cat});
+            this.$el.append(view.render().el);
+        },
+
+        render: function() {
+            this.$el.html("");
+            this.collection.each(this.addCat, this);
+        }
+    }),
+
+        my_view = new CatCollectionView ();
+});
+
+
+
