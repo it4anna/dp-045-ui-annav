24564a674a4fac476b72abebec029a49e769a07d
AnnaVostrykova <it4anna@gmail.com>
2013-11-27 18:57:05 +0200

    scaffold with updates Gemfile (backbone+, underscore+)

---
+++ rails/stickers/app/controllers/app_controller.rb
@@ -0,0 +1,4 @@
+class AppController < ApplicationController
+  def start
+  end
+end
---
+++ rails/stickers/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stickers::Application.config.secret_key_base = '712c9db2e6a36835d139b8da16b0b4a8c4ed01bfdaef28e91dcb887f8075fcc7ff85e9d5f056b3ab4f280edfdf83ee38d68795a4c0b6f5612c4dfd52955a039f'
